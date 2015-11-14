6b8a081c9ea76e78ca7c89a8bdcd03d721f37569
Joshun <joshua.oleary@btinternet.com>
2015-11-14 15:01:35 +0000

    Enable object properties to be queried

--- TilesetLoader.py
+++ TilesetLoader.py
@@ -6,12 +6,25 @@ class TilesetLoader(object):
 
     def __init__(self, tileset_file):
         self.__tiled_map = pytmx.load_pygame(tileset_file)
+        self.__objects = []
+
+        for layer in self.__tiled_map.layers:
+            for obj in layer:
+                self.__objects.append(obj)
 
     def draw(self, screen):
         for layer in self.__tiled_map.layers:
             for obj in layer:
                 image_rect = pg.Rect(obj.x, obj.y, obj.width, obj.height)
                 screen.blit(obj.image, image_rect)
+
+    def get_objects(self):
+        return self.__objects
+
+    def get_object_properties(self):
+        for obj in self.__objects:
+            print(obj.properties)
+
 
 
 
