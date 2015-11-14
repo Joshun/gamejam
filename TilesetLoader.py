537fd7950bcb2e42e4bbdecfa5cc945d3bdbaf56
Joshun <joshua.oleary@btinternet.com>
2015-11-14 15:59:23 +0000

    Can now query for specific properties

--- TilesetLoader.py
+++ TilesetLoader.py
@@ -1,29 +1,42 @@
 import pygame as pg
 import pytmx
 
+from pytmx.util_pygame import load_pygame
 
 class TilesetLoader(object):
 
     def __init__(self, tileset_file):
-        self.__tiled_map = pytmx.load_pygame(tileset_file)
-        self.__objects = []
+        self.__tiled_map = load_pygame(tileset_file)
+        self.__tile_properties = []
 
-        for layer in self.__tiled_map.layers:
-            for obj in layer:
-                self.__objects.append(obj)
+        for layer_num in range(len(self.__tiled_map.layers)):
+            for gid_property in self.__tiled_map.get_tile_properties_by_layer(layer_num):
+                self.__tile_properties.append(gid_property)
+
+        print(self.get_all_tiles_with_property("pstart"))
+
+
+
+    def get_all_tiles_with_property(self, tproperty):
+        for gid, properties in self.__tile_properties:
+            if tproperty in properties:
+                return (gid,properties)
+
 
     def draw(self, screen):
-        for layer in self.__tiled_map.layers:
-            for obj in layer:
-                image_rect = pg.Rect(obj.x, obj.y, obj.width, obj.height)
-                screen.blit(obj.image, image_rect)
+        for layer in self.__tiled_map.visible_layers:
+            for x, y, image in layer.tiles():
+                image_rect = pg.Rect(x, y, 16, 16)
+                screen.blit(image, image_rect)
+            # break
 
-    def get_objects(self):
-        return self.__objects
-
-    def get_object_properties(self):
-        for obj in self.__objects:
-            print(obj.properties)
+    #
+    # def get_objects(self):
+    #     return self.__objects
+    #
+    # def get_object_properties(self):
+    #     for obj in self.__objects:
+    #         print(obj.properties)
 
 
 
