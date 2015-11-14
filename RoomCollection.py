f4277a9769f8b709d48268e791e328cb539d6858
Joshun <joshua.oleary@btinternet.com>
2015-11-14 18:00:50 +0000

    Get RoomCollection to parse JSON correctly

--- RoomCollection.py
+++ RoomCollection.py
@@ -7,6 +7,11 @@ class RoomCollection(object):
         with open(description_file) as df:
             json_data = json.load(df)
             room_keys = json_data.keys()
-            for room in room_keys:
-                # room_description =
-                pass
+            for room_id in room_keys:
+                print(room_id)
+                room_name = json_data[room_id]["room_name"]
+                room_description = json_data[room_id]["description"]
+                room_tilemap = json_data[room_id]["tile_map"]
+
+
+RoomCollection("RoomDescriptions.json")
