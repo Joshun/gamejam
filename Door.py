9d701fdbbf2692b51f46432c21ff51ad56c1dd0e
Joshun <joshua.oleary@btinternet.com>
2015-11-14 21:57:44 +0000

    Get entry points to work

--- Door.py
+++ Door.py
@@ -1,14 +1,9 @@
 class Door(object):
-    def __init__(self, next_room, coords, room_collection):
+    def __init__(self, next_room):
         self.__next_room = next_room
-        self.__coords = coords
 
     def get_next_room(self):
         return self.__next_room
 
     def player_action(self, player, room_collection):
         pass
-
-
-
-
--- Room.py
+++ Room.py
@@ -1,5 +1,7 @@
 from Door import *
 
+import sys
+
 class Room(object):
     """Base class for a Room"""
     def __init__(self, tiled_map):
@@ -8,16 +10,28 @@ class Room(object):
         self.__room_collection = []
         self.__tiled_map_objects = self.__tiled_map.get_objects()
         self.__character_tiles = self.__tiled_map.get_all_objects_with_property("character")
+        self.__entry_point = self.__init_entry_point()
         self.__doors = self.__setup_doors(self.__tiled_map.get_all_objects_with_property("next_room"))
 
         # self.__doors = self.__setup_doors(self.__tiled_map.get_all_tiles_with_property("next_room"))
 
         print(self.__doors)
 
+    def __init_entry_point(self):
+        entry_point_test = self.__tiled_map.get_all_objects_with_property("entry_point")
+        if len(entry_point_test) == 0:
+            # apologies for the Steve Maddock style code
+            print("Error! Room ", self, "has no entry point.")
+            sys.exit(1)
+        else:
+            entry_point = entry_point_test[0]
+            print("Setting player entry point to", entry_point.x, entry_point.y)
+            return entry_point
+
     def __setup_doors(self, doors_list):
         doors = []
         for obj in doors_list:
-            doors.append(Door(obj.properties["next_room"], (obj.x, obj.y), self.__room_collection))
+            doors.append(Door(obj.properties["next_room"]))
         return doors
 
     def set_room_collection(self, room_collection):
