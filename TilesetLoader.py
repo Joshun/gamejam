d50fd993bba99c7629399a08bf43d5c2f905b701
Joshun <joshua.oleary@btinternet.com>
2015-11-14 16:56:32 +0000

    Get the stupid tileset thing to actually draw!!!!!!

--- TilesetLoader.py
+++ TilesetLoader.py
@@ -6,7 +6,7 @@ from pytmx.util_pygame import load_pygame
 class TilesetLoader(object):
 
     def __init__(self, tileset_file):
-        self.__tiled_map = load_pygame(tileset_file)
+        self.__tiled_map = load_pygame(tileset_file, pixelalpha=True)
         self.__tile_properties = []
 
         for layer_num in range(len(self.__tiled_map.layers)):
@@ -24,12 +24,15 @@ class TilesetLoader(object):
 
 
     def draw(self, screen):
+
         for layer in self.__tiled_map.visible_layers:
             for x, y, image in layer.tiles():
-                image_rect = pg.Rect(x, y, 16, 16)
-                screen.blit(image, image_rect)
-            # break
+                image_rect = pg.Rect(x*16, y*16, 16, 16)
 
+                screen.blit(image, image_rect)
+                # pg.draw.rect(screen,pg.Color(255,255,255), image_rect)
+                # pg.Surface.blit(image, screen, image_rect)
+                # pg.image.save(image, "out")
     #
     # def get_objects(self):
     #     return self.__objects
