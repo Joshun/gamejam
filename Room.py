c2095400011fb5369325e9be663409b6a6882b5f
Jack Deadman <jackdeadman@icloud.com>
2015-11-14 14:00:23 +0000

    Add basic room classes

---
+++ Room.py
@@ -0,0 +1,11 @@
+class Room(object):
+  """Base class for a Room"""
+  def __init__(self, tiled_map):
+    self.tiled_map = tiled_map
+
+  def draw():
+    pass
+
+  def update():
+    pass
+    
---
+++ ScrollableRoom.py
@@ -0,0 +1,5 @@
+class ScrollableRoom(Room):
+  """A room with a camera that does moves with the player"""
+  def __init__(self):
+    pass
+    
