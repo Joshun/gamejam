6ce03b5fe305cf209100698ac3b02ddd800973dd
Joshun <joshua.oleary@btinternet.com>
2015-11-14 21:07:37 +0000

    improve door

--- Door.py
+++ Door.py
@@ -1,7 +1,14 @@
 class Door(object):
-    def __init__(self, next_room):
+    def __init__(self, next_room, coords):
         self.__next_room = next_room
+        self.__coords = coords
 
     def get_next_room(self):
         return self.__next_room
+
+    def player_action(self, coords):
+        if coords == self.__coords:
+
+
+
 
