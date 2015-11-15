3c24ee6637f1ac6c68b8dcd090911cc439f75c8d
Jack Deadman <jackdeadman@icloud.com>
2015-11-15 04:07:45 +0000

    Made danny's rubbish code betterererer with numpy

--- Entities/Enemy.py
+++ Entities/Enemy.py
@@ -2,7 +2,7 @@ from Entities.Character import Character
 from abc import abstractmethod, ABCMeta
 import math
 import random
-
+import numpy as np
 
 class Enemy(Character, metaclass=ABCMeta):
     def __init__(self, start_pos, speed, anim_speed, health, weapon, sprite):
@@ -10,39 +10,19 @@ class Enemy(Character, metaclass=ABCMeta):
         self.weapon = weapon
         self.move_away = random.randint(0, 1)
 
+
     def update_pos(self, player, delta_time):
-        pos_x, pos_y = self.get_centre()
-        player_pos_x, player_pos_y = player.get_centre()
+        player_pos = np.array(player.get_centre()).astype(int)
+        enemy_pos = np.array(self.get_centre()).astype(int)
 
-        x_diff = pos_x - player_pos_x
-        y_diff = pos_y - player_pos_y
-        dist = math.sqrt(pow(x_diff, 2) + pow(y_diff, 2))
+        diff_vec = player_pos - enemy_pos
+        move_vector = np.zeros((2,)).astype(int)
+        move_vector[np.argmax(np.abs(diff_vec))] = np.sign(diff_vec[np.argmax(np.abs(diff_vec))])
 
-        if dist - 2 <= self.weapon.range <= dist + 2:
-            # print("should be shooting")
-            self.weapon.shoot(delta_time)
-            # print(delta_time)
-        elif dist > self.weapon.range:
-            scale = 1
-            if abs(x_diff) > abs(y_diff):
-                if x_diff > 0:
-                    scale = -1
-                self.rect.x += self.speed * scale
-            elif abs(x_diff) < abs(y_diff):
-                if y_diff > 0:
-                    scale = -1
-                self.rect.y += self.speed * scale
-        elif dist < self.weapon.range:
-            scale = 1
-            if self.move_away == 0:
-                if pos_x < player_pos_x:
-                    scale = -1
-                self.rect.x += self.speed * scale
-            else:
-                if pos_y < player_pos_y:
-                    scale = -1
-                self.rect.y += self.speed * scale
-
+        if np.linalg.norm(diff_vec, ord=1) > 50:
+            self.rect.x += move_vector[0]
+            self.rect.y += move_vector[1]
+        
     @abstractmethod
     def update(self, delta, player):
         pass
