89fe5940f26b999f8cc0f1eb7cfd3e92d1984f1d
endomatrix <mattbarbers@btinternet.com>
2015-11-15 00:00:38 +0000

    Updated Generic

--- graphics/objects/entities/gen_entitysheet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm entitysheet.png
-echo "If this makes more than one tilesheet, then change the -tile 8x8 part of the script to have bigger dimensions."
-montage entity*.png -tile 8x8 -geometry 16x16+0+0 -background transparent entitysheet.png
