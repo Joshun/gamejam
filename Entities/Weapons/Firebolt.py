33aa12e1c6d32b4bd8282318f58fbb73d9553535
Joshun <joshua.oleary@btinternet.com>
2015-11-15 12:13:50 +0000

    Attempting to fix the enemies

--- Entities/Weapons/Firebolt.py
+++ Entities/Weapons/Firebolt.py
@@ -12,7 +12,7 @@ class Firebolt(RangedWeapon):
         self.speed = 2
         self.bullet_surface = pygame.Surface((16, 16))
         self.direction = direction
-        self.bullet = Projectile(self.weapon_x, self.weapon_y, self.speed, self.direction, self.damage, pygame.image.load("graphics/sprites/entities/entity16_fireball.png").convert(), self.range)
+        self.bullet = Projectile(self.weapon_x, self.weapon_y, self.speed, self.direction, self.damage, pygame.image.load("graphics/sprites/entities/entity16_elemental.png").convert(), self.range)
         self.__elapsed_time = 0
         self.__previous_time = 0
         self.__update_time = 10
